b5800368894b45dd74063012917dd6845bafc4f4
Robbie Gordon <oddballishere@gmail.com>
2024-02-08 12:50:58 -0800

    Initial commit

---
+++ Runner.py
@@ -0,0 +1,10 @@
+from FileRead import FileRead
+from FileWrite import FileWrite
+from FileEncrypter import FileEncrypter
+from FileDecrypter import FileDecrypter
+
+fileRead = FileRead("AllWords.txt")
+fileRead.readFirstLineFromFile()
+
+
+
